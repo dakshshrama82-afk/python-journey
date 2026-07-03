81254b04400ff456deede3e71fee486a6149cb79
Daksh Sharma <dakshshrama82@gmail.com>
2026-07-03 17:38:55 +0530

    Add problems for simple calculator and even/odd check to Day 1 practice

--- Day1/Day1_practice.py
+++ Day1/Day1_practice.py
@@ -5,3 +5,32 @@ name = input("enter the nmae:")
 print("sir",name,"what is your age")
 age = int(input("enter the age:"))
 print("Hello sir ",name,", you are",age,"years old!")
+
+#Problem 2: Simple Calculator,Ask the user for two numbers and print their sum, difference, product, and quotient.
+no_1 = int(input("enter the first no:"))
+no_2 = int(input("enter the 2nd no:"))
+
+sum = no_1 +no_2
+difference = no_1 - no_2
+product = no_1 * no_2
+if no_2 == 0:
+    print("division by zero is not possible")
+else:
+    quotient = no_1/no_2
+
+print("answer sheet:")         
+print("sum=",sum)
+print("difference=",difference)
+print("product=",product)
+print("quotient=",quotient)
+
+#Problem 3: Even or Odd
+
+a = int(input("enter the no"))
+
+if a%2 == int:
+    print("the no was even")
+else:
+    print("the no was odd")
+
+
