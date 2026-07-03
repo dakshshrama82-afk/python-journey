15735f5643f16f031e99aec05839ae0ffd97f91b
Daksh Sharma <dakshshrama82@gmail.com>
2026-07-03 16:41:30 +0530

    Add Day 1 practice quaction1 file

---
+++ Day1/Day1_practice.py
@@ -0,0 +1,7 @@
+# Day 1 phython practice
+# Problem 1: Greeting with Input,Ask the user for their name and age, then print:Hello <name>, you are <age> years old!
+print("Hello my name is aman sir,what is your name")
+name = input("enter the nmae:")
+print("sir",name,"what is your age")
+age = int(input("enter the age:"))
+print("Hello sir ",name,", you are",age,"years old!")
