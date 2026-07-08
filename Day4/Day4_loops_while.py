c2795f409686f106e13e8ffe28fccc3c9a5112c3
Daksh Sharma <dakshshrama82@gmail.com>
2026-07-09 00:16:03 +0530

    solving 3 loops problem in while statement

---
+++ Day4/Day4_loops_while.py
@@ -0,0 +1,30 @@
+# practice quaction for while loop
+#1 Print numbers from 1 to 100.
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+#2 Print numbers from 100 to 1.
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+
+#3 Print the multiplication table of a number n.
+
+i = 5
+while i <= 100:
+    print(i)
+    i += 5
+    
+
+#4 Print the elements of the following list using a loop:
+
+#[1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+#5 Search for a number x in this tuple using loop:
+
+#[1 4 9 16 25, 36, 49, 64, 81,100]
