6aa3511b6e3e89adc684ec689c7116de3bbcd76d
Daksh Sharma <dakshshrama82@gmail.com>
2026-07-10 20:45:39 +0530

    Refactor Day 4 loops file: add print statements for answers and correct multiplication table logic

--- Day4/Day4_loops_while.py
+++ Day4/Day4_loops_while.py
@@ -1,6 +1,7 @@
 # practice quaction for while loop
 #1 Print numbers from 1 to 100.
 
+print("ans 1:")
 i = 1
 while i <= 100:
     print(i)
@@ -8,6 +9,7 @@ while i <= 100:
 
 #2 Print numbers from 100 to 1.
 
+print("ans 2:")
 i = 100
 while i >= 1:
     print(i)
@@ -15,16 +17,18 @@ while i >= 1:
 
 #3 Print the multiplication table of a number n.
 
-i = 5
-while i <= 100:
-    print(i)
-    i += 5
+print("ans 3:")
+i = 1
+while i<=10:
+    print(17*i)
+    i += 1
     
 
 #4 Print the elements of the following list using a loop:
 
 #[1, 4, 9, 16, 25, 36, 49, 64, 81,100]
 
+print("ans 4:")
 given_list = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
 
 i = 1
@@ -36,3 +40,15 @@ while i <= 10 :
 
 #[1 4 9 16 25, 36, 49, 64, 81,100]
 
+print("ans 5:")
+number = (1,4,9,16,25,36,49,64,81,100)
+
+x = int(input("enter the value:"))
+i = 0
+while i < len(number):
+    if x == number[i]:
+        print("found at index:",i)
+    elif number[i] != x:
+        print("not,found",i)
+    i += 1
+    
