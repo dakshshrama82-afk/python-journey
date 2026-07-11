89587593edb3f44dca05717e81b4aef1bd1f4b89
Daksh Sharma <dakshshrama82@gmail.com>
2026-07-11 17:29:06 +0530

    Enhance search functionality in Day 4 loops file to display index of found element

--- Day4/Day4_loops_for.py
+++ Day4/Day4_loops_for.py
@@ -17,11 +17,13 @@ else:
 num = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
 
 x = int(input("enter the no:"))
+idx = 0
 for y in num:
     if x == y :
-        print("found the element:",x)
+        print("found the element ",x," at index:",idx)
         print("END OF LOOP")
         break
+    idx += 1
 else:
     print("do not exist")
     print("END OF LOOP")
