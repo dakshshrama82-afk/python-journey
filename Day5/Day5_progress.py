13c00426c90db9b1011b551ceb3429e319ff9b77
Daksh Sharma <dakshshrama82@gmail.com>
2026-07-14 19:29:52 +0530

    Refactor word frequency and length categorization logic in Day 5 progress script

--- Day5/Day5_progress.py
+++ Day5/Day5_progress.py
@@ -21,4 +21,27 @@ for word_str in sentence_list:
 word_dic = {}
 for word in singel_word:
     if word in word_dic:
-        
+        word_dic[word] += 1
+    else:
+        word_dic[word] = 1
+word_sat = {
+
+}
+word_sat = set(singel_word)
+
+
+print("word frequency:",word_dic)
+print("unique word:",word_sat)
+
+
+word_len = {"short":0, "medium":0, "long":0}
+
+for word in word_sat:   # loop through unique words
+    if len(word) < 4:
+        word_len["short"] += 1
+    elif len(word) <= 6:
+        word_len["medium"] += 1
+    else:
+        word_len["long"] += 1
+
+print("Word length categories:", word_len)
