89c1af851227fc1a2dd65aa51efaa4b0c37baa75
Daksh Sharma <dakshshrama82@gmail.com>
2026-07-20 00:10:42 +0530

    adding result and the other thing

--- Day5/Day5_progress.py
+++ Day5/Day5_progress.py
@@ -1,4 +1,5 @@
-#taking the input from the person till he/she enter done
+#taking the input from the person till he/she enter done.
+#also convert the text to lowercase.
 sentence_list =[]
 while True:
     collect = input("enter the sentence:")
@@ -6,10 +7,15 @@ while True:
     if collection != "done":
         sentence_list.append(collection)
     elif collection == "done":
+        if not sentence_list:
+            print("you did not enter any thing")
         break
 
+#removing punctuation from the text.
+# using len so that if we change the item count in punctation code do not break.
 singel_word = []
-punctuation = ["!","@","#","$",",","?","."]
+sentence_set = []
+punctuation = ["!","@","#","$",",","?",".",":",";","'","-"]
 for word_str in sentence_list:
         i = 0
         while i < len(punctuation):
@@ -17,6 +23,9 @@ for word_str in sentence_list:
          word_str = word_str.replace(item,"")
          i += 1
         singel_word.extend(word_str.split())
+        sentence_set.append(set(word_str.split()))
+
+#increasing the count and counting the word or repeted word.
 
 word_dic = {}
 for word in singel_word:
@@ -24,24 +33,48 @@ for word in singel_word:
         word_dic[word] += 1
     else:
         word_dic[word] = 1
-word_sat = {
 
-}
+#storing all unique word.
+
+
 word_sat = set(singel_word)
 
-
-print("word frequency:",word_dic)
-print("unique word:",word_sat)
-
+#total no of word use in accourding to there lengeth.
 
 word_len = {"short":0, "medium":0, "long":0}
 
-for word in word_sat:   # loop through unique words
+for word in word_sat:   
     if len(word) < 4:
         word_len["short"] += 1
     elif len(word) <= 6:
         word_len["medium"] += 1
     else:
         word_len["long"] += 1
+common_word = {}
+if not sentence_list:
+    pass
+else:
+    common_word = sentence_set[0]
 
-print("Word length categories:", word_len)
+
+
+for s in sentence_set:
+    common_word = common_word.intersection(s)
+
+most_u_word = ""
+max_u_count = 0
+for max_word,max_count in word_dic.items():
+    if max_count >= max_u_count:
+        most_u_word = max_word
+        max_u_count = max_count
+
+
+
+print("---summary report showing:---")
+print("Total number of words entered:", len(singel_word))
+print("Total number of unique words:",len(word_sat))
+print("The most frequently used word:",most_u_word,"and its count:",max_u_count)
+print("common words:", common_word)
+print("count of short word:",word_len["short"])
+print("count of medium word:",word_len["medium"])
+print("count of larg word:",word_len["long"])
