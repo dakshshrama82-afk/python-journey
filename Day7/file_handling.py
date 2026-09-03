f984504c4f60fceec6999952bdf94f1b3fa705d4
Daksh Sharma <dakshshrama82@gmail.com>
2026-09-04 00:03:50 +0530

    Add file handling examples for different modes in Python

---
+++ Day7/file_handling.py
@@ -0,0 +1,24 @@
+with open("hie.txt","a") as f:
+    data1 = f.write("hi i am thr king")
+    print(data1)
+
+# r+ mode
+with open("demo.txt","r+") as f:
+    data3 = f.read()
+    data3 = f.write("hi its me")
+    print(data3)
+
+# w+ mode
+with open("demo.txt","w+") as f:
+    data4 = f.write("hi")
+    data4 = f.read()
+    print(data4)
+
+
+
+
+# a+ mode 
+with open("demo.txt","a+") as f:
+    data5 = f.write("\nhi it is again me")
+    data5 = f.read()
+    print(data5)
